e5b49c5463fb4de5c568dff608927c0baa1d6106
Eswar <79842705+EswarNani123@users.noreply.github.com>
2025-03-05 13:53:22 +0530

    Update start_zergpool.sh

--- start_zergpool.sh
+++ start_zergpool.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Start SRBMiner-MULTI with Parameters: --algorithm $ALGO --pool $POOL_ADDRESS --wallet DOGE:$WALLET_USER.$HOSTNAME#Jumper --password $PASSWORD"
-./SRBMiner-MULTI --algorithm $ALGO --pool $POOL_ADDRESS --wallet DOGE:$WALLET_USER.$HOSTNAME#Jumper --password $PASSWORD
+echo "Start SRBMiner-MULTI with Parameters: --algorithm $ALGO --pool $POOL_ADDRESS --wallet DOGE:DOGE:DKR83hbfCGGzNyZb73LCs1Pz9RegBpCUtB.unmineable_worker_yphqgmbd#1bz8-v3i2 --password $PASSWORD"
+./SRBMiner-MULTI --algorithm $ALGO --pool $POOL_ADDRESS --wallet DOGE:DKR83hbfCGGzNyZb73LCs1Pz9RegBpCUtB.unmineable_worker_yphqgmbd#1bz8-v3i2 --password $PASSWORD
