86568350adb10d6d29b3598487419286f3c583ce
Eswar <79842705+EswarNani123@users.noreply.github.com>
2025-03-05 13:42:21 +0530

    Update start_zergpool.sh

--- start_zergpool.sh
+++ start_zergpool.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cpu_threads=$(grep -c '^processor' /proc/cpuinfo)
-echo "Start SRBMiner-MULTI with Parameters: --algorithm $ALGO --pool $POOL_ADDRESS --wallet LTC:$WALLET_USER.$HOSTNAME#Jumper --password $PASSWORD $EXTRAS --cpu-threads $cpu_threads"
-./SRBMiner-MULTI --algorithm $ALGO --pool $POOL_ADDRESS --wallet LTC:$WALLET_USER.$HOSTNAME#Jumper --password $PASSWORD $EXTRAS --cpu-threads $cpu_threads
+echo "Start SRBMiner-MULTI with Parameters: --algorithm $ALGO --pool $POOL_ADDRESS --wallet DOGE:$WALLET_USER.$HOSTNAME#Jumper --password $PASSWORD"
+./SRBMiner-MULTI --algorithm $ALGO --pool $POOL_ADDRESS --wallet DOGE:$WALLET_USER.$HOSTNAME#Jumper --password $PASSWORD
