6b0b4c12b3fb4efa3df79f84c6940d65922d9a6e
Eswar <79842705+EswarNani123@users.noreply.github.com>
2025-03-05 14:00:26 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ version="2.7.9"
 registries=("docker.io" "ghcr.io" "quay.io")
 
 # Build the image
-docker build . --build-arg VERSION_TAG=$version --tag ${registries[0]}/cniweb/$image:$version
+docker build . --build-arg VERSION_TAG=$version --tag ${registries[0]}/EswarNani123/$image:$version
 
 # Check if the command was successful
 if [ $? -ne 0 ]; then
@@ -17,11 +17,11 @@ echo "Docker build succeeded!"
 
 # Tag and push the images
 for registry in "${registries[@]}"; do
-  docker tag ${registries[0]}/cniweb/$image:$version $registry/cniweb/$image:$version
-  docker tag ${registries[0]}/cniweb/$image:$version $registry/cniweb/$image:latest
+  docker tag ${registries[0]}/EswarNani123/$image:$version $registry/EswarNani123/$image:$version
+  docker tag ${registries[0]}/EswarNani123/$image:$version $registry/EswarNani123/$image:latest
   
   # Push both versioned and latest tags
-  docker push $registry/cniweb/$image:$version
-  docker push $registry/cniweb/$image:latest
+  docker push $registry/EswarNani123/$image:$version
+  docker push $registry/EswarNani123/$image:latest
 done
 
